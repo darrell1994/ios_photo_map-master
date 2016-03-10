764b454249018e752be213eff26d66b971369ece
Darrell Shi <darrell_authentic@yahoo.com>
2016-03-10 16:35:03 -0500

    Milestone 3

--- Photo Map/PhotoMapViewController.swift
+++ Photo Map/PhotoMapViewController.swift
@@ -7,29 +7,60 @@
 //
 
 import UIKit
+import MapKit
 
-class PhotoMapViewController: UIViewController {
+class PhotoMapViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBOutlet weak var cameraContainerView: UIView!
+    @IBOutlet weak var mapView: MKMapView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        cameraContainerView.clipsToBounds = true
+        cameraContainerView.layer.cornerRadius = cameraContainerView.frame.height/2
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: "onTappingCamera:")
+        cameraContainerView.userInteractionEnabled = true
+        cameraContainerView.addGestureRecognizer(tapGesture)
+        
+        let sfRegion = MKCoordinateRegionMake(CLLocationCoordinate2DMake(37.783333, -122.416667),
+            MKCoordinateSpanMake(0.1, 0.1))
+        mapView.setRegion(sfRegion, animated: false)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func onTappingCamera(gesture: UITapGestureRecognizer) {
+        print("Camera tapped")
+//        let cameraView = gesture.view!
+//        let frame = view.frame
+//        
+//        UIView.animateWithDuration(0.2, delay: 0, options: [UIViewAnimationOptions.Autoreverse, UIViewAnimationOptions.CurveEaseIn], animations: { () -> Void in
+//            cameraView.transform = CGAffineTransformMakeScale(1.2, 1.2)
+//            }, completion: { (Bool) -> Void in
+////                cameraView.frame = frame
+////                cameraView.layer.cornerRadius = cameraView.frame.height/2
+//            }
+//)
+        
+        
+        let vc = UIImagePickerController()
+        vc.delegate = self
+        vc.allowsEditing = true
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera) {
+            vc.sourceType = UIImagePickerControllerSourceType.Camera
+        } else {
+            vc.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        }
+        
+        self.presentViewController(vc, animated: true, completion: nil)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    var selectedImage: UIImage?
+    
+    func imagePickerController(picker: UIImagePickerController,
+        didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+            let editedImage = info[UIImagePickerControllerEditedImage] as! UIImage
+            selectedImage = editedImage
+            dismissViewControllerAnimated(true, completion: nil)
+            self.performSegueWithIdentifier("tagSegue", sender: nil)
     }
-    */
-
 }
